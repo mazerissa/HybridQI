06f920d92fa1e0cd46daa7e0b1f0cddb76626ff0
mazerissa <youcefelghoul@yahoo.com>
2026-03-04 16:56:10 +0100

    made the module

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,2 +1,13 @@
-class MLP:
-    pass
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #Architecture
+        self.funcition_01 = nn.Linear(784, 128)
+        self.funcition_02 = nn.Linear(128, 64)
+        self.funcition_03 = nn.Linear(62, 10)
+        #Rectified Linear Unit or ReLU
+        self.relu = nn.ReLU()
+
