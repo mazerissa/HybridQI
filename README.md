60452bc7368c72e71801069f66be7b325b76ffeb
mazerissa <youcefelghoul@yahoo.com>
2026-03-05 21:58:17 +0100

    testing more

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Solving basic gradient descent
 
+θt+1​=θt​−η∇θ​L(θ)
 
-<h2 style="text-align: center">θt+1​=θt​−η∇θ​L(θ)</h2>
+# ReLU
+
+$f(x) = \max(0, x)$
+
+# Matrix Multiplication
+
+$y = Wx + b$
+
+# Cross-Entropy Loss
+
+$L = -\sum y \log(\hat{y})$
+
+# Backpropagation & Calculus
+
+$\frac{\partial L}{\partial W}$
+
+# Gradient Descent
+
+$W_{new} = W_{old} - \eta \nabla L$
+
--- src/models/mlp.py
+++ src/models/mlp.py
@@ -7,7 +7,14 @@ class MLP(nn.Module):
         #Architecture
         self.funcition_01 = nn.Linear(784, 128)
         self.funcition_02 = nn.Linear(128, 64)
-        self.funcition_03 = nn.Linear(62, 10)
+        self.funcition_03 = nn.Linear(64, 10)
         #Rectified Linear Unit or ReLU
         self.relu = nn.ReLU()
+
+    def forward(self, m):
+        m = self.relu(self.funcition_01(m))
+        m = self.relu(self.funcition_02(m))
+        m = self.funcition_03(m)
+        return m
+    
 
